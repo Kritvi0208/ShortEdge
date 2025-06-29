adf2648099724d8204d58cc280cd5ec7d9820407
Ritvika <ritvikakay@gmail.com>
2025-06-29 10:59:11 +0530

    Update url.go

--- model/url.go
+++ model/url.go
@@ -7,12 +7,12 @@ type URL struct {
 	LongURL    string     `json:"long_url"`
 	CreatedAt  time.Time  `json:"created_at"`
 	Visibility string     `json:"visibility"` // "public" or "private"
-	ExpiresAt  *time.Time `json:"expires_at"` // ⏰ New field
+	ExpiresAt  *time.Time `json:"expires_at"` // 
 }
 
 type ShortenRequest struct {
 	LongURL    string     `json:"long_url"`
 	CustomCode string     `json:"custom_code"` // Optional
 	Visibility string     `json:"visibility"`  // public / private
-	ExpiresAt  *time.Time `json:"expires_at"`  // ⏰ Optional expiry
+	ExpiresAt  *time.Time `json:"expires_at"`  // Optional 
 }
